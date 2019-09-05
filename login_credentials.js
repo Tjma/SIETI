b37aa1dce3084377b737c8a3c7d20f13587b0701
adlumanta <adlumanta@up.edu.ph>
2019-09-05 17:45:40 +0800

    get data from My Course History

---
+++ login_credentials.js
@@ -0,0 +1,4 @@
+module.exports = {
+    username: 'adlumanta@up.edu.ph',
+    password: 'Sais_123456789',
+  }
--- scraper_sais.js
+++ scraper_sais.js
@@ -1,20 +1,11 @@
 const puppeteer = require('puppeteer');
-const C = require('./constants');
-const USERNAME_SELECTOR = '#userid';
-const PASSWORD_SELECTOR = '#pwd';
-const CTA_SELECTOR = '#login > button';
-const SELF_SERV = '#ptnav2pglt > #ptnav2pgltbody > #ptnav2tree #fldra_CO_EMPLOYEE_SELF_SERVICE';
-const WORKLIST = '#ptnav2pglt > #ptnav2pgltbody > #ptnav2tree #fldra_PT_WORKLIST';
-const MY_FAVORITES = '#ptnav2pglt > #ptnav2pgltbody > #ptnav2tree #fldra_MYFAVORITES';
-const USER_DEFAULTS = '#ptnav2pglt > #ptnav2pgltbody > #ptnav2tree #crefli_HC_OPR_DEFAULT_CS_GBL > a';
-const CHANGE_PASS = '#ptnav2pglt > #ptnav2pgltbody > #ptnav2tree #crefli_PT_CHANGE_PASSWORD_GBL > a';
-const MY_PERSONALIZATIONS = '#ptnav2pglt > #ptnav2pgltbody > #ptnav2tree #crefli_PT_USER_SELF_PERSONAL_GBL > a';
-const SEARCH_BAR = '#MENU_Data > #ptnav2pglt > #ptnav2srch #ptnav2srchinput';
-const SEARCH_BUTTON = '#MENU_Data > #ptnav2pglt > #ptnav2srch #ptnav2go';
+const C = require('./login_credentials');
 
-const VIEW_MY_GRADES = '#ptifrmcontent > #ptifrmtarget > #ptifrmtgtframe >';
+const INPUT_USERNAME_SELECTOR = '#userid';
+const INPUT_PASSWORD_SELECTOR = '#pwd';
+const BUTTON_LOGIN_SELECTOR = '#login > button';
 
-
+const A_SELF_SERVICE = '#ptnav2pglt > #ptnav2pgltbody > #ptnav2tree #fldra_CO_EMPLOYEE_SELF_SERVICE';
 
 async function startBrowser() {
   const browser = await puppeteer.launch({
@@ -38,62 +29,45 @@ async function playTest(url) {
   });
 
   await page.goto(url);
-  await page.click(USERNAME_SELECTOR);
+  await page.click(INPUT_USERNAME_SELECTOR);
   await page.waitFor(3000);
   await page.keyboard.type(C.username);
   await page.waitFor(3000);
-  await page.click(PASSWORD_SELECTOR);
+  await page.click(INPUT_PASSWORD_SELECTOR);
   await page.waitFor(3000);
   await page.keyboard.type(C.password);
   await page.waitFor(3000);
-  await page.click(CTA_SELECTOR);
+  await page.click(BUTTON_LOGIN_SELECTOR);
   await page.waitFor(3000);
  
   // **** FOR SELF SERVICE ****
-  // await page.waitForSelector(SELF_SERV);
-  // await page.click(SELF_SERV);
-  
-  // **** FOR WORKLIST ****
-  // await page.waitForSelector(WORKLIST);
-  // await page.click(WORKLIST);
+  await page.waitForSelector(A_SELF_SERVICE);
+  await page.click(A_SELF_SERVICE);
+  await page.waitFor(5000);
 
-  // **** FOR MY FAVORITES ****
-  // await page.waitForSelector(MY_FAVORITES);
-  // await page.click(MY_FAVORITES);
-  
-  // **** FOR USER DEFAULTS ****
-  // await page.waitForSelector(USER_DEFAULTS);
-  // await page.click(USER_DEFAULTS);
+  // **** SELF SERVICE > CLASS SEARCH ****
 
-  // **** FOR CHANGE MY PASSWORD ****
-  // await page.waitForSelector(CHANGE_PASS);
-  // await page.click(CHANGE_PASS);
+  // **** FRAME ****
+  await page.goto('https://sais.up.edu.ph/psp/ps/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSS_MY_CRSEHIST.GBL?PORTALPARAM_PTCNAV=HC_SSS_MY_CRSEHIST_GBL&EOPP.SCNode=HRMS&EOPP.SCPortal=EMPLOYEE&EOPP.SCName=CO_EMPLOYEE_SELF_SERVICE&EOPP.SCLabel=Self%20Service&EOPP.SCPTfname=CO_EMPLOYEE_SELF_SERVICE&FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_ACADEMIC_RECORDS.HC_SSS_MY_CRSEHIST_GBL&IsFolder=false');
+  dumpFrameTree(page.mainFrame(), '');
+  const frame = page.frames().find(frame => frame.name() === 'TargetContent');
+  // const text = await frame.$eval('table.PSLEVEL1GRIDWBO > tbody', element => element.textContent);
+  const data = await frame.evaluate(() => {
+    const grades = Array.from(document.querySelectorAll('table.PSLEVEL1GRIDWBO > tbody'))
+    return grades.map(data => data.innerText);
+  })
 
-  // **** FOR MY PERSONALIZATIONS ****
-  // await page.waitForSelector(MY_PERSONALIZATIONS);
-  // await page.click(MY_PERSONALIZATIONS);
-  
-  // ***** FOR SEARCHING *******  
-  // await page.waitForSelector(SEARCH_BAR); 
-  // await page.click(SEARCH_BAR);
-  // await page.keyboard.type(C.searchWord);
-  // await page.click(SEARCH_BUTTON); 
+  console.log("FRAME CONTENT ", data);
 
-  // **** SCRAPE ANNOUNCEMENT ****
-  const headings = await page.evaluate(() => 
-    Array.from(document.querySelectorAll("td#ptpgltbody_ADMN_E_ANNOUNCE_HMPG > span#ADMN_E_ANNOUNCE_HMPG_Data > div > table > tbody > tr > td > h2, h3")).map((h) => h.innerText.trim())
-  )
-  console.log("HEADINGS = > ", headings);
-
-  const announcements = await page.evaluate(() => 
-    Array.from(document.querySelectorAll("td#ptpgltbody_ADMN_E_ANNOUNCE_HMPG > span#ADMN_E_ANNOUNCE_HMPG_Data > div > table > tbody > tr > td > p")).map((p) => p.innerText.trim())
-  )
-  console.log("P - >", announcements);
-
+  // **** SCREENSHOT ****
+  await page.screenshot({path: 'screenshot.png'});
   console.log("DONE");
+}
 
-  await page.waitFor(3000);
-  await page.screenshot({path: 'sais_scrape.png'});
+function dumpFrameTree(frame, indent) {
+  for(let child of frame.childFrames()) {
+    dumpFrameTree(child, indent + " ");
+  }
 }
 
 (async () => {
